90f633d705b3f1a6c810895232918cb4bb331932
cvnxsolution <cvnxsolution@gmail.com>
2025-07-30 19:52:45 +0630

    intial spring boot setup

---
+++ win-commerce-backend/src/test/java/com/cvnxsolution/wincommercebackend/WinCommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvnxsolution.wincommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinCommerceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
